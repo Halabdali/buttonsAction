7accdef49f61d044536a518cf0edebfb40ef4b31
unknown <hamzaa1779@gmail>
2020-06-29 05:25:33 +0300

    Initial commit

---
+++ Actions.php
@@ -0,0 +1,35 @@
+<?php
+
+if(isset($_POST['up'])) {
+	echo 'Forward';
+}
+
+if(isset($_POST['right'])) {
+	echo 'Right';
+}
+
+if(isset($_POST['down'])) {
+	echo 'Down';
+}
+
+if(isset($_POST['left'])) {
+	echo 'Left';
+}
+
+if(isset($_POST['tri'])) {
+	echo 'Triangle';
+}
+
+if(isset($_POST['sqr'])) {
+	echo 'Square';
+}
+
+if(isset($_POST['x'])) {
+	echo 'X';
+}
+
+if(isset($_POST['o'])) {
+	echo 'O';
+}
+
+?>
